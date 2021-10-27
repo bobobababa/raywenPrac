2425160d7786836b64b7b30258f6a191e0f3124a
bobobababa <31619939+bobobababa@users.noreply.github.com>
2021-10-27 18:08:42 +1100

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by ZHENGYI TANG on 27/10/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
